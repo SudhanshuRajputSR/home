7c68655d9bb0af338c705b7b2e7744b352248c8d
SudhanshuRajputSR <44068817+SudhanshuRajputSR@users.noreply.github.com>
2020-05-08 22:36:14 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,20 +1,3 @@
-%%time
-
-epochs = 15
-steps_per_epoch = train_generator.n//train_generator.batch_size
-validation_steps = validation_generator.n//validation_generator.batch_size
-
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
-                              patience=2, min_lr=0.00001, mode='auto')
-checkpoint = ModelCheckpoint("model_weights.h5", monitor='val_accuracy',
-                             save_weights_only=True, mode='max', verbose=1)
-callbacks = [PlotLossesCallback(), checkpoint, reduce_lr]
-
-history = model.fit(
-    x=train_generator,
-    steps_per_epoch=steps_per_epoch,
-    epochs=epochs,
-    validation_data = validation_generator,
-    validation_steps = validation_steps,
-    callbacks=callbacks
-)
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)

3b31cc2153017677fcc42f6eae4682adc945dfcd
SudhanshuRajputSR <44068817+SudhanshuRajputSR@users.noreply.github.com>
2020-05-08 22:33:42 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,51 +1,20 @@
-# Initialising the CNN
-model = Sequential()
+%%time
 
-# 1 - Convolution
-model.add(Conv2D(64,(3,3), padding='same', input_shape=(48, 48,1)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+epochs = 15
+steps_per_epoch = train_generator.n//train_generator.batch_size
+validation_steps = validation_generator.n//validation_generator.batch_size
 
-# 2nd Convolution layer
-model.add(Conv2D(128,(5,5), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                              patience=2, min_lr=0.00001, mode='auto')
+checkpoint = ModelCheckpoint("model_weights.h5", monitor='val_accuracy',
+                             save_weights_only=True, mode='max', verbose=1)
+callbacks = [PlotLossesCallback(), checkpoint, reduce_lr]
 
-# 3rd Convolution layer
-model.add(Conv2D(512,(3,3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-# 4th Convolution layer
-model.add(Conv2D(512,(3,3), padding='same'))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-# Flattening
-model.add(Flatten())
-
-# Fully connected layer 1st layer
-model.add(Dense(256))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Dropout(0.25))
-
-# Fully connected layer 2nd layer
-model.add(Dense(512))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Dropout(0.25))
-
-model.add(Dense(7, activation='softmax'))
-
-opt = Adam(lr=0.0005)
-model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-model.summary()
+history = model.fit(
+    x=train_generator,
+    steps_per_epoch=steps_per_epoch,
+    epochs=epochs,
+    validation_data = validation_generator,
+    validation_steps = validation_steps,
+    callbacks=callbacks
+)
